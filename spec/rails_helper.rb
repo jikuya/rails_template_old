8f5b251d49e1fab1d7102e6e579cd96dc8ea83d5
jikuya-kunihiko <jikuya.kunihiko@dena.jp>
2015-02-25 18:09:46 +0900

    Deviseの設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,5 +61,11 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # Devise
+  # include Warden::Test::Helpers
+  # config.include Devise::TestHelpers, type: :controller
+  # config.extend ControllerMacros, :type => :controller
+  # config.include RequestHelpers, :type => :request
 end
 
